c5d6570f12d67dfec494f9798326c10dba303492
md3sIam <mvp3199@gmail.com>
2019-01-02 05:34:57 +0300

    add algos for undirectional map

--- python/AlgoChooser.py
+++ python/AlgoChooser.py
@@ -1,11 +1,21 @@
 import classes.Algorithms as Algs
 
 algorithms = list()
+
+# Directed algorithms
 algorithms.append(Algs.dijkstra_way_by_ids)
 algorithms.append(Algs.dijkstra_early_stop_way_by_ids)
 algorithms.append(Algs.bidirectional_dijkstra_by_ids)
 algorithms.append(Algs.astar_by_ids)
 algorithms.append(Algs.bidirectional_astar_by_ids)
+
+# Undirected algorithms
+algorithms.append(Algs.dijkstra_way_un_by_ids)
+algorithms.append(Algs.dijkstra_early_stop_way_un_by_ids)
+algorithms.append(Algs.bidirectional_dijkstra_un_by_ids)
+algorithms.append(Algs.astar_un_by_ids)
+algorithms.append(Algs.bidirectional_astar_un_by_ids)
+
 
 
 def get_algorithm_by_id(alg_id):
