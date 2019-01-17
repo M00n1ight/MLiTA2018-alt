a437acd48314ef4389af12c1717225975cb5c119
Semen Tseb <semen.tseb@gmail.com>
2019-01-17 21:45:28 +0300

    Small fix
    Typo  shall not pass.

--- launcher_win.bat
+++ launcher_win.bat
@@ -3,7 +3,17 @@
 @start cmd /k node index.js
 @echo London, Paris, SPb3, Toronto
 @echo.
+:do
 @set /p city="Choose the city: "
+:while
+@if "%city%" == "London" (goto next) else (goto one)
+:one
+@if "%city%" == "Paris" (goto next) else (goto two)
+:two
+@if "%city%" == "SPb3" (goto next) else (goto three)
+:three
+@if "%city%" == "Toronto" (goto next) else (goto do)
+:next
 @cd ..\
 @cd .\python
 @explorer http://localhost:8080
